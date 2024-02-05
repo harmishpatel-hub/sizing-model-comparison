a406f90d90f937388aecd3e35e32736ef12d80ff
harmishpatel21 <harmish2111patel@gmail.com>
2024-02-05 11:58:00 -0500

    multiple length based width vs depth chart added

--- src/charts/draw_scatter.py
+++ src/charts/draw_scatter.py
@@ -1,18 +1,24 @@
 from plotly import graph_objects as go 
 
-def drawPhysicsChart(dataframe, colX, colY, tick=5):
+def drawPhysicsChart(dataframe, colX, colY, colColor, tick=5):
     fig = go.Figure()
-    fig.add_trace(
-        go.Scatter(
-            x = dataframe[colX],
-            y = dataframe[colY],
-            mode = 'markers+lines'
+    for len in dataframe[colColor].unique():
+        fig.add_trace(
+            go.Scatter(
+                x = dataframe[dataframe[colColor]==len][colX],
+                y = dataframe[dataframe[colColor]==len][colY],
+                mode = 'markers+lines',
+                name = f"{colColor.upper()} {len} [in]",
+                showlegend=True
+            )
         )
-    )
     fig.update_layout(
-        title = f"{colX.upper()} [in] VS {colY.upper()} [%]",
-        width = 1000,
-        height = 800,
+        title = f"""{colX.upper()} [in] VS {colY.upper()} [%] 
+        | WT={dataframe['wt'].unique()[0]} 
+        | EXT/INT = {'EXTERNAL' if dataframe['Ext/Int'].unique()[0] == 1 else 'INTERNAL'} 
+        | PEAK VALUE={dataframe['peak_value'].unique()[0]}""",
+        width = 1500,
+        height = 1200,
         xaxis_title = f"{colX.upper()}",
         yaxis_title = f"{colY.upper()} [%]",
         xaxis = dict(dtick = 0.05),
